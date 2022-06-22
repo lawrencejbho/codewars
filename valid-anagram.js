34b474e33a64d75e0b4e05eb04684b76d542289d
Lawrence Ho <lawrence.jb.ho@gmail.com>
2022-06-21 18:23:08 -0700

    valid-anagram

---
+++ valid-anagram.js
@@ -0,0 +1,55 @@
+(s = "anagram"), (t = "nagaram");
+
+function anagram() {
+  if (s.length != t.length) {
+    return false;
+  }
+  const hash = {};
+  for (let i = 0; i < s.length; i++) {
+    if (hash[s[i]]) {
+      hash[s[i]] += 1;
+    } else {
+      hash[s[i]] = 1;
+    }
+  }
+  for (let i = 0; i < t.length; i++) {
+    if (hash[t[i]]) {
+      hash[t[i]] -= 1;
+      if (hash[t[i]] < 0) {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(anagram());
+
+//better way to do it, that is more concise with less comparisons
+
+function anagram2() {
+  if (s.length !== t.length) {
+    return false;
+  }
+
+  const dict = {};
+
+  for (let i = 0; i < s.length; i++) {
+    if (!dict[s[i]]) {
+      dict[s[i]] = 1;
+    } else {
+      dict[s[i]]++;
+    }
+  }
+
+  for (let i = 0; i < t.length; i++) {
+    if (!dict[t[i]] || dict[t[i]] <= 0) {
+      return false;
+    }
+    dict[t[i]]--;
+  }
+
+  return true;
+}
