c11a2b2ec3bd01b11ff4df980a03b25220a47bb7
Lawrence Ho <lawrence.jb.ho@gmail.com>
2022-07-28 20:15:31 -0700

    reverse polish

--- evaluate-reverse-polish-notation.js
+++ evaluate-reverse-polish-notation.js
@@ -1,3 +1,10 @@
+// this problem is pretty fun given that it's a stack, the key here is converting the expression to an operator that
+// can be used later.  Not sure why we need Math.trunc here, but if I don't use it I get the wrong answer on some inputs
+// in terms of the algo, we can push values into our stack.  whenever there is an math operator, we pop two values and
+// apply the operator as the new Value.  We can then push this new Value into our stack and continue.  Complexity is O(n)
+// as we only need to traverse the entire starting array once as our stack will continue to handle for us as we
+// continue applying operators and push new Values.
+
 const tokens = ["2", "1", "+", "3", "*"];
 const tokens2 = ["4", "13", "5", "/", "+"];
 const tokens3 = [
@@ -19,6 +26,7 @@ const tokens3 = [
 var evalRPN = function (tokens) {
   const arr = [];
 
+  // this is the magic
   const mathDefined = {
     "+": (x, y) => x + y,
     "-": (x, y) => x - y,
@@ -32,8 +40,8 @@ var evalRPN = function (tokens) {
     if (tokens[i] < 200) {
       arr.push(tokens[i]);
     } else {
-      let num2 = parseInt(arr.pop());
-      let num1 = parseInt(arr.pop());
+      const num2 = parseInt(arr.pop());
+      const num1 = parseInt(arr.pop());
       //   console.log(num1);
       //   console.log(num2);
       let newVal = mathDefined[tokens[i]](num1, num2);
