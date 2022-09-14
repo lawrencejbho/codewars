57a854d017b163c378475b7a8e2d3506a1dc6c8d
Lawrence Ho <lawrence.jb.ho@gmail.com>
2022-09-14 12:17:24 -0700

    some comments

--- k-closest-points-to-origin.js
+++ k-closest-points-to-origin.js
@@ -1,19 +1,24 @@
+/* 
+Javascript has weird answers for this one on Leetcode.  
+
+We use min priority queue so that we can get the smallest elements, and it looks like the last parameter we pass 
+in the enqueue is the priority.  So calculate the distance on each point and then enqueue.  After that just 
+dequeue accordingly and push it into our array to be returned.
+
+Complexity is O(n) to heapify  + O(klogn) to dequeue so O(klogn).  
+
+If you were to use sort, it would be n(logn) 
+*/
+
 var kClosest = function (points, k) {
   const mpq = new MinPriorityQueue();
   const res = [];
-  for (let i = 0; i < points.length; i++) {
-    let x = points[i][0];
-    let y = points[i][1];
-    let distance = [];
-    distance = [Math.pow(x, 2) * Math.pow(y, 2), x, y];
-    mpq.enqueue(distance);
+  for (let point of points) {
+    let distance = Math.pow(point[0], 2) + Math.pow(point[1], 2);
+    mpq.enqueue(point, distance);
   }
 
-  while (mpq.size() > k) {
-    mpq.dequeue().element;
-  }
-
-  for (let i = 0; i < k; i++) {
+  while (res.length < k) {
     res.push(mpq.dequeue().element);
   }
 
