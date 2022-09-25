c0595cc029df6b7ebbd9aaf75e2d7b99df088e3f
Lawrence Ho <lawrence.jb.ho@gmail.com>
2022-09-24 17:03:39 -0700

    pacific atlantic completed

--- pacific-atlantic-water-flow.js
+++ pacific-atlantic-water-flow.js
@@ -6,39 +6,44 @@ var pacificAtlantic = function (heights) {
   let rows = heights.length;
   let cols = heights[0].length;
   let res = [];
-  let pacific = false;
-  let atlantic = false;
+  let pac = new Set();
+  let atl = new Set();
 
-  function dfs(x, y, prev) {
-    if (x < 0 || y < 0) {
-      pacific = true;
+  function dfs(x, y, prev, visit) {
+    if (
+      x < 0 ||
+      y < 0 ||
+      x >= rows ||
+      y >= cols ||
+      visit.has(`${x},${y}`) ||
+      heights[x][y] < prev
+    ) {
       return;
     }
-    if (x >= rows || y >= cols) {
-      atlantic = true;
-      return;
-    }
-    if (heights[x][y] == -1) return;
 
-    if (prev > heights[x][y]) return;
+    visit.add(`${x},${y}`);
 
-    prev = heights[x][y];
-    heights[x][y] = -1;
+    dfs(x + 1, y, heights[x][y], visit);
+    dfs(x - 1, y, heights[x][y], visit);
+    dfs(x, y + 1, heights[x][y], visit);
+    dfs(x, y - 1, heights[x][y], visit);
+  }
 
-    dfs(x + 1, y, prev);
-    dfs(x - 1, y, prev);
-    dfs(x, y + 1, prev);
-    dfs(x, y - 1, prev);
-    heights[x][y] = prev;
-    return false;
+  for (let r = 0; r < rows; r++) {
+    dfs(r, 0, -1, pac);
+    dfs(r, cols - 1, -1, atl);
+  }
+
+  for (let c = 0; c < cols; c++) {
+    dfs(0, c, -1, pac);
+    dfs(rows - 1, c, -1, atl);
   }
 
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
-      dfs(i, j, 0);
-      if (atlantic && pacific) res.push([i, j]);
-      pacific = false;
-      atlantic = false;
+      if (pac.has(`${i},${j}`) && atl.has(`${i},${j}`)) {
+        res.push([i, j]);
+      }
     }
   }
   return res;
